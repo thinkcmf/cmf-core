d8936707f726e73e7240e2ba3cb970ea451b096f
zxxjjforever <zxxjjforever@163.com>
2019-07-31 07:57:01 +0800

    修复API中文件url转化错误

--- src/App.php
+++ src/App.php
@@ -274,9 +274,6 @@ class App extends Container
 
         // 路由初始化
         $this->routeInit();
-
-        // 监听app_init
-        $this->hook->listen('app_init');
     }
 
     /**
@@ -423,6 +420,11 @@ class App extends Container
         try {
             // 初始化应用
             $this->initialize();
+
+            if (!$this->initialized) {
+                // 监听app_init
+                $this->hook->listen('app_init');
+            }
 
             if ($this->bindModule) {
                 // 模块/控制器绑定
